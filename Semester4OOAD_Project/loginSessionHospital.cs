858a1da4fb56f8463db44687563f47155ccec1df
haideraliabdulqayyum <haider.ha.ali.99@gmail.com>
2018-05-07 23:37:15 +0500

    m
    m

---
+++ Semester4OOAD_Project/loginSessionHospital.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semester4OOAD_Project
+{
+    class loginSessionHospital
+    {
+        public static network.classHospital session = null;
+    }
+}
